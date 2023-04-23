e5bbfa9f069b76cc6eb8bb443742d5a43e9b4a32
Kayce Basques <kaycebasques@gmail.com>
2023-04-23 16:01:05 -0700

    Add TODO

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ def install_requires():
     with open('requirements.txt', 'r') as f:
         return [line.strip() for line in f.readlines()]
 
+# TODO: Make it possible to use the builder without modifying the extensions list.
+# https://www.sphinx-doc.org/en/master/development/builders.html
 setup(
     name='sphinx-embeddings-builder',
     version='0.1',
