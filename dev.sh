8e220d891cf12782a5ef17156024279e8876a398
Kayce Basques <kaycebasques@gmail.com>
2023-04-23 16:10:55 -0700

    Continue automating the dev workflow

--- dev.sh
+++ dev.sh
@@ -1,3 +1,5 @@
+working_directory=$(pwd)
+
 if [ ! -d ~/.venv ]; then
     mkdir ~/.venv
 fi
@@ -13,9 +15,12 @@ fi
 
 source ~/.venv/sphinx-embeddings-builder/bin/activate
 cd /tmp/sphinx
-python3 -m pip uninstall sphinx-embeddings-builder
+python3 -m pip uninstall --yes sphinx-embeddings-builder
 python3 -m pip install sphinx-embeddings-builder
 python3 -m pip install -U sphinx-embeddings-builder
 python3 -m pip install -e .
-sphinx-build -M embeddings ./doc ./build/sphinx -W --keep-going
+# TODO: Replace `html` with `embeddings`
+# https://www.sphinx-doc.org/en/master/man/sphinx-build.html#cmdoption-sphinx-build-E
+sphinx-build -M html ./doc ./build/sphinx -W --keep-going -E
 deactivate
+cd $working_directory
