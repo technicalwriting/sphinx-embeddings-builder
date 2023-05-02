8d9906efa48a6fe3550301a4bcc76787a1c48a7d
Kayce Basques <kaycebasques@gmail.com>
2023-05-01 18:45:54 -0700

    Miscellaneous cleanup

--- sphinx-embeddings-builder/__init__.py
+++ sphinx-embeddings-builder/__init__.py
@@ -51,10 +51,6 @@ class EmbeddingsBuilder(Builder):
                 'text': text,
                 'embedding': self.generate_embedding(text)
             }
-        # self.data[docname] = {
-        #     # 'tokens': self.count_tokens(text),
-        #     'checksum': md5(text.encode('utf-8')).hexdigest()
-        # }
 
     def finish(self):
         data_path = path.join(self.outdir, 'embeddings.json')
@@ -72,34 +68,3 @@ def setup(app):
         'parallel_read_safe': True,
         'parallel_write_safe': True,
     }
-
-
-# from docutils import nodes
-# from hashlib import md5
-# from json import dumps
-# import tiktoken
-# 
-# encoding = tiktoken.encoding_for_model('gpt-4')
-# data = {}
-# 
-# def estimate_token_count(text):
-#     return len(encoding.encode(text))
-# 
-# def generate_embeddings(app, doctree, docname):
-#     data[docname] = {}
-#     for section in doctree.traverse(nodes.section):
-#         text = section.astext()
-#         checksum = md5(text.encode('utf-8')).hexdigest()
-#         data[docname][checksum] = {
-#             'text': text,
-#             'tokens': estimate_token_count(text)
-#         }
-#     print(data)
-# 
-# def setup(app):
-#     app.connect('doctree-resolved', generate_embeddings)
-#     return {
-#         'version': '0.1',
-#         'parallel_read_safe': True,
-#         'parallel_write_safe': True,
-#     }
